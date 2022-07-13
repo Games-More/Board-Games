1928c74e7ca2f49577745962721ae4679c9a37dd
GuzmanMartinez <67272145+GuzmanMartinez@users.noreply.github.com>
2022-07-13 20:41:21 +0100

    head
    Componente head añadido

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeadComponent } from './head/head.component';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+      HeadComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
