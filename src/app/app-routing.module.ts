91ebabd76cebae69de00fd04954f7c282dc79a5d
Guille <guifers23@gmail.com>
2022-07-13 22:01:03 +0200

    Routing del menú

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TheMindComponent } from "../games/the-Mind/the-Mind.component";
+import { MenuComponent } from "../app/menu/menu.component";
 
-const routes: Routes = [{path:'the-Mind',component:TheMindComponent}];
+const routes: Routes = [
+  {path:'',component:MenuComponent},
+  {path:'menu',component:MenuComponent},
+  {path:'the-Mind',component:TheMindComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
