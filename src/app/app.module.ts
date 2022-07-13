ac36e978979c4d0b1264c364df43511dd090fd71
Guille <guifers23@gmail.com>
2022-07-13 21:36:33 +0200

    Añadidos componentes Menú y Footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [		
+    AppComponent,
+      MenuComponent,
+      FooterComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
